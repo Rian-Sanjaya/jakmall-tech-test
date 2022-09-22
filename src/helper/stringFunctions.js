8af1383b7bf8c16a40eb6d9f4de05630038566e5
Rian Sanjaya <rian.sj@gmail.com>
2022-09-22 21:13:14 +0700

    generate random orderID

---
+++ src/helper/stringFunctions.js
@@ -0,0 +1,7 @@
+function randomSpecificString(length, chars) {
+  var result = '';
+  for (var i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
+  return result;
+}
+
+export { randomSpecificString };
